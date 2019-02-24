188bd13c919c1d866168ca90164d7fb52c7d5592
Danny McAteer <mcateerd2@gmail.com>
2019-02-24 18:49:15 -0500

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -24,11 +24,11 @@ function analyze() {
     xhr.onload = function(e) {
         if (this.readyState === 4) {
             var response = JSON.parse(e.target.responseText);
-            if (${response['result']} === 'most_beautiful') {
-                el('result-label').innerHTML = `Yes, that's her!`;
-            } else {
-                el('result-label').innerHTML = `Nope. Try again!`;
-            }
+                if (response === 'most_beautiful') {
+                    el('result-label').innerHTML = `Yes, that's her!`;
+                } else {
+                    el('result-label').innerHTML = `Nope. Try again!`;
+                }
         }
         el('analyze-button').innerHTML = 'Analyze';
     }
